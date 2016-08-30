b30c05646b7c67f42560a3eb5937ce5a7e793b67
Yvonne Nduta <yvonnendutaw@users.noreply.github.com>
2016-08-30 09:58:31 +0300

    Create solvemefirst.py

---
+++ solvemefirst.py
@@ -0,0 +1,9 @@
+def solveMeFirst(a,b):
+#declares the function that returns the sum of the numbers.
+    C = a+b
+    return C
+#calling the function to add any numbers input by the user 
+num1 = int(raw_input(''))
+num2 = int(raw_input(''))
+res = solveMeFirst(num1,num2)
+print res
